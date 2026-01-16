933bcecd92751adff74869f44b1502352e4ee714
flonix8 <flstanek@googlemail.com>
2026-01-16 14:22:45 +0000

    Fix modem test

--- tests/test_modem_tcl_ike41ve1.py
+++ tests/test_modem_tcl_ike41ve1.py
@@ -1,7 +1,5 @@
 import logging
 
-import sys
-
 import pytest
 
 from msu_manager.config import PingConfig
@@ -16,7 +14,10 @@ async def test_tcl_ike41ve1_modem_reconnect(request, caplog):
     if 'tests/test_modem_tcl_ike41ve1.py' not in request.config.args:
         pytest.skip("Skipping TCL IKE41VE1 modem tests (specify tests/test_modem_tcl_ike41ve1.py explicitly to enable)")
     
-    ping_instance = Ping(PingConfig(target='1.1.1.1'))
+    ping_instance = Ping(PingConfig(
+        target='1.1.1.1',
+        interface='wwan0',
+    ))
     modem = TCL_IKE41VE1(ping_instance)
     await modem.reconnect()
 
