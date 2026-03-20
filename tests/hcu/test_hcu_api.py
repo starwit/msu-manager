5e933c6494d6e30f3c0502adad04bf6bfd640ab3
flonix8 <flstanek@googlemail.com>
2026-03-20 12:08:55 +0100

    Fix tests

--- tests/hcu/test_hcu_api.py
+++ tests/hcu/test_hcu_api.py
@@ -6,28 +6,24 @@ from fastapi import FastAPI
 from httpx import ASGITransport, AsyncClient
 
 
-@pytest_asyncio.fixture
-async def test_client(default_testee, write_serial_input):
+@pytest.mark.asyncio
+async def test_shutdown_inhibit_ongoing(default_testee, tmp_shutdown_file, write_serial_input):
     app = FastAPI()
     default_testee.add_routes(app.router)
     await default_testee.run()
-
+    await asyncio.sleep(0.01)  # Give the skill some time to start and set up the serial connection
+        
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url='http://test'
-    ) as client:
-        yield client
-        
-    await default_testee.close()
-    
+    ) as test_client:    
+        await write_serial_input(b'{"type":"SHUTDOWN"}\n')
+        await asyncio.sleep(0.01)
+        await test_client.get('/shutdown/inhibit/2')
+        assert not tmp_shutdown_file.exists()
+        await asyncio.sleep(1.5)
+        assert not tmp_shutdown_file.exists()
+        await asyncio.sleep(1)
+        assert tmp_shutdown_file.exists()
 
-@pytest.mark.asyncio
-async def test_shutdown_inhibit_ongoing(tmp_shutdown_file, test_client, write_serial_input):
-    await write_serial_input(b'{"type":"SHUTDOWN"}\n')
-    await asyncio.sleep(0.01)
-    await test_client.get('/shutdown/inhibit/2')
-    assert not tmp_shutdown_file.exists()
-    await asyncio.sleep(1.5)
-    assert not tmp_shutdown_file.exists()
-    await asyncio.sleep(1)
-    assert tmp_shutdown_file.exists()
+    await default_testee.close()
 
